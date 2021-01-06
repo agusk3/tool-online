f506db6a41d743b7c10ef4c5ebd329874d4cf70c
agusk3 <layen2508@gmail.com>
2021-01-06 23:10:39 +0700

    first commit

--- end.php
+++
@@ -1,7 +0,0 @@
-<div class="panel panel-primary"><div class="panel-heading">Thông Tin</div><div class="list-group">
-<div class="list-group-item">• Email: <a href="mailto:nhox04@gmail.com">Nhox04@Gmail.Com</a></div>
-
-<div class="list-group-item">• Đ/C: Hiếu Pờ Rồ - Forum.xViet.Mobi</div>
-</div></div></div>
-</div>
-<footer><div id="footer" class='panel panel-default'><div class="row"><p class="text-muted text-center"><p class="text-muted text-center">Copyright © 2016 <a href="/" title="">Pro321.CF</a></p></p></div></footer></div></body></html>
