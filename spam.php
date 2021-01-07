76fcbd16c19d086940f6be03bdef62a20fb1571f
agusk3 <61266725+agusk3@users.noreply.github.com>
2021-01-07 19:50:44 +0700

    Update spam.php

--- spam.php
+++ spam.php
@@ -1,4 +1,9 @@
 <?php 
+echo'
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SpamMessage.Ga Trang Spam Tin Nhắn Miễn Phí</title>";
 $phone=$_GET[ "phone"]; 
 $amount=$_GET[ "amount"];
 $header = array(
@@ -9,6 +14,7 @@ $header = array(
 $data = "ver=0&manufacturer_id=3c94e2b344d907fb&device_name=Google G011A&device_model=G011A&os_sdk=22&fk=first#key&session=&os_version=5.1.1&device_type=2&version_number=10&mobile_login=".$phone;
 $login = "https://apib2cm.mytvnet.vn/v8/vnptid/send-otp";
 if(isset($_GET[ "phone"])){ 
+echo "Send Messages To Phone: ".$phone;
     if($amount=="" ){
         for($i=0; $i<=100; $i++){ 
           $ch = curl_init();
